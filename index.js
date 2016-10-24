f98dfcc0c611783ae567d44adb24e71cb324a401
A P note <b@b.b>
2016-10-24 21:12:51 +0300

    initial file list recieving

--- index.js
+++ index.js
@@ -2,3 +2,21 @@
 "use strict";
 
 console.log("global node module v1.0.1");
+
+var fs = require('fs');
+var path = require('path');
+
+const folder = '.';
+var fileNames = [];
+
+fs.readdir(folder, (err, files) => {
+	files.forEach(file => {
+		console.log(file);
+		fileNames.push(file.toString());
+	});
+	interpreterList();
+})
+
+function interpreterList(){
+	console.log("files: "+fileNames);
+}
